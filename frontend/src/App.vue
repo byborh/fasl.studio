21f58667f4f73c4803489a43dbb361d899be996d
rh_bybo <byborh@gmail.com>
2023-12-25 16:32:05 +0100

    Créer d'une petite main page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,21 +2,35 @@
   <div id="app">
     <!--HEADER DU SITE-->
     <headerC />
-
+    
     <!--BODY DU SITE-->
-    <div class="pinterest-img">
+    <div class="body">
+      <!--VIDEO-->
+      <video playsinline autoplay muted loop poster="">
+        <source src="./img/logo/fasl-studio-video.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+      </video>
+        
+      <!--Photo stylé à scroll / Pinterest
+      <div class="pinterest-img">
 
-    	<div class="pinterest-img-deux-alignes">
-    				<img src="../public/img/408210154_902853758043645_7692857693830266755_n.jpg" alt="Photo vintage">
-    				<div class="texteCentrer">Il y en a qui regardent, il y en a qui <b>portent</b>.</div>
-      </div>
-      
-    	<div class="pinterest-img-deux-alignes">
-    				<img src="../public/img/380854559_828498658740460_3155866281269219205_n.jpg" alt="Photo vintage">
-    				<div class="texteCentrer">Chapitre</div>
-			</div>
+        <div class="pinterest-img-deux-alignes">
+          <img src="./img/380854559_828498658740460_3155866281269219205_n.jpg" alt="Photo vintage">
+          <div class="texteCentrer">Il y en a qui regardent, il y en a qui <b>portent</b>.</div>
+        </div>
+
+        <div class="pinterest-img-deux-alignes">
+          <img src="./img/365392420_1660761207734436_3165609358977754361_n.jpg" alt="Photo vintage">
+          <div class="texteCentrer">Chapitre</div>
+        </div>
+
+      </div>-->
 
     </div>
+
+
+
+
 
     <!--FOOTER DU SITE-->
     <footerC />
--- frontend/src/components/headerC.vue
+++ frontend/src/components/headerC.vue
@@ -1,34 +1,30 @@
 <template>
-    <nav>
-    <div class="navbar">
-        <div class="logo">
-          <h1>{{ marque_nom_arabe }}</h1>
+
+	<header>
+        <div class="navbar">
+          <div class="container nav-container">
+              <input class="checkbox" type="checkbox" name="" id="" />
+              <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+              </div>  
+            <div class="logo">
+              <h1>{{ marque_nom_arabe }}</h1>
+            </div>
+
+            <div class="menu-items">
+              <li><a href="my-page.php"><img id="profil" src="../img/logo/profil.png"></a></li>
+              <li><a href="#">NOUVEAUTÉS</a></li>
+              <li><a href="#">VÊTEMENTS</a></li>
+              <li><a href="#">ACCESSOIRES</a></li>
+              <li><a href="#">À PROPOS</a></li>
+              <li><a href="#">CONTACT</a></li>
+            </div>
+
+          </div>
         </div>
-        <div class="container nav-container">
-            <input class="checkbox" type="checkbox" name="" id="" />
-            <div class="hamburger-lines">
-            <span class="line line1"></span>
-            <span class="line line2"></span>
-            <span class="line line3"></span>
-        </div>  
-        
-        <div class="form-place">
-            <form method="post" action="/check-user">
-                <input type="text" name="username" placeholder="Chercher...">
-                <button type="submit">ﻣ</button>
-            </form>
-        </div>
-        <div class="menu-items">
-          <li><a href="/">Home</a></li>
-          <li><a href="/nouveautes">Nouveautés</a></li>
-          <li><a href="/vetements">Vêtements</a></li>
-          <li><a href="/accessoires">Accessoires</a></li>
-          <li><a href="/about">A Propos</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </div>
-      </div>
-    </div>
-</nav>
+	</header>
 </template>
 
 <script>
