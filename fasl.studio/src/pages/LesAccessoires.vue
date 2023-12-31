48d1e96b7b8e36ac0ff5b9518cc575fc0ca8c574
rh_bybo <byborh@gmail.com>
2023-12-31 01:41:50 +0100

    Créer une nouvelle interface (effectué : main page et à propos page)

--- fasl.studio/src/pages/LesAccessoires.vue
+++ fasl.studio/src/pages/LesAccessoires.vue
@@ -1,8 +1,37 @@
 <template>
     <h1>
-        Les Accessoires
+        H1 - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat vitae tempora, saepe perferendis non, rem, porro animi laboriosam ullam nam modi possimus magnam commodi cumque atque? Ducimus error debitis pariatur.
     </h1>
-    
+    <h2>
+        H2 -Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore qui fugiat, temporibus omnis, nobis nisi impedit cupiditate nemo natus commodi optio voluptate laboriosam nulla dicta architecto, nam eos. Maiores, minima!
+    </h2>
+    <h3>
+        H3 -Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis atque magni natus! Quam illum earum consectetur, vitae quasi fuga eos itaque excepturi, pariatur consequatur harum delectus aliquid iusto ipsam atque.
+    </h3>
+    <h4>
+        H4 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptate quibusdam non atque a molestiae consequuntur nihil nulla laudantium eius. Accusamus adipisci ad aut culpa, aperiam consequuntur mollitia? Odio, harum.
+    </h4>
+    <h5>
+        H5 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quod accusantium provident corporis. Dolore aperiam esse accusamus nisi laudantium corrupti illum adipisci, itaque tempore aliquam? Necessitatibus sequi odit nostrum rerum.
+    </h5>
+    <h6>
+        H6 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora esse iste soluta veritatis cum ipsa distinctio nam inventore? Pariatur aliquid voluptatibus accusantium a cumque at aliquam aspernatur optio hic temporibus.
+    </h6>
+
+    <p>
+        P - Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto ab alias quos doloremque! Accusantium quas unde delectus aperiam impedit fuga totam quam iure nulla. Fuga dolores alias dolore sunt officia!
+    </p>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 </template>
 
 <script>
