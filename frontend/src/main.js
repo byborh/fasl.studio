7cc8bbd233ef8d08b29f17f60186f9b2b607f72c
kazakh_rh <kazakh_rh@DESKTOP-BBQMNQK.localdomain>
2024-01-23 19:39:48 +0100

    Supprimer le dossier frontend - car : 0 utilité

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-// Importer les css
-import './assets/main.css'
-import './assets/headerC.css'
-import './assets/footerC.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
